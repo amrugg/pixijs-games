3d8beed26d7833a8ef3f7539b3fd99fba900f49e
amrugg <ruggkids@gmail.com>
2023-12-22 06:40:14 -0600

    Saving progress

--- esper/usefulFunctions.js
+++ esper/usefulFunctions.js
@@ -248,10 +248,10 @@ function hitTestRectangle(r1, r2, offsetX, offsetY) {
     hit = false;
 
     //Find the center points of each sprite
-    r1.centerX = r1.x + r1.width / 2;
-    r1.centerY = r1.y + r1.height / 2;
-    r2.centerX = r2.x + r2.width / 2;
-    r2.centerY = r2.y + r2.height / 2;
+    r1.centerX = r1.x// + r1.width / 2;
+    r1.centerY = r1.y// + r1.height / 2;
+    r2.centerX = r2.x// + r2.width / 2;
+    r2.centerY = r2.y// + r2.height / 2;
 
     //Find the half-widths and half-heights of each sprite
     r1.halfWidth = r1.width / 2;
